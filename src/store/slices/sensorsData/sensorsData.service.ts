484c6f8a3f7f65b77ed08fa0060d3329dbe254a9
Mariana Vallejos <marivalleandrea@gmail.com>
2025-04-18 14:15:24 -0400

    Add axios config

--- src/store/slices/sensorsData/sensorsData.service.ts
+++ src/store/slices/sensorsData/sensorsData.service.ts
@@ -1,39 +1,42 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import { sensorsDataActions } from "./sensorsData.slice";
 import { sensorsData } from "../../../types/sensorsData";
-import sensorData from '../../../assets/data/sensors.json';
 import { formatDate } from "../../../utils";
+import api from "../../../api/axios";
 
 interface BackendSensorData {
-    id: number;
-    temperature: number;
-    humidity: number;
-    createdAt: string;
-    updatedAt: string;
-  }
-  
-  interface BackendResponse {
-    data: BackendSensorData[];
-    message: string;
-    ok: boolean;
-  }
-  
-export const getAllData = () => async (dispatch: Dispatch) => {
-    try {
-        // llamada a ep
-        dispatch(sensorsDataActions.setIsLoading(true));
-        const backendData = JSON.parse(JSON.stringify(sensorData)) as BackendResponse;
-
-        const transformedData: sensorsData[] = await backendData.data.map(item => ({
-          temperature: item.temperature,
-          humidity: item.humidity,
-          date: formatDate(item.createdAt)
-        }));
-
-        dispatch(sensorsDataActions.setData(transformedData));
-    } catch (error) {
-        console.error("Error fetching sensor data:", error);
-    }finally{
-        dispatch(sensorsDataActions.setIsLoading(false));
-    }
+  id: number;
+  temperature: number;
+  humidity: number;
+  createdAt: string;
+  updatedAt: string;
 }
+
+interface BackendResponse {
+  data: BackendSensorData[];
+  message: string;
+  ok: boolean;
+}
+
+export const getAllData = () => async (dispatch: Dispatch) => {
+  try {
+    // llamada a ep
+    dispatch(sensorsDataActions.setIsLoading(true));
+
+    const response = await api.get("/sensors");
+    const backendData: BackendResponse = response.data;
+
+    const transformedData: sensorsData[] = backendData.data.map((item) => ({
+      temperature: item.temperature,
+      humidity: item.humidity,
+      date: formatDate(item.createdAt),
+    }));
+
+    dispatch(sensorsDataActions.setData(transformedData));
+  } catch (error) {
+    console.error("Error fetching sensor data:", error);
+    dispatch(sensorsDataActions.setMessage("Failed to load sensor data"));
+  } finally {
+    dispatch(sensorsDataActions.setIsLoading(false));
+  }
+};
