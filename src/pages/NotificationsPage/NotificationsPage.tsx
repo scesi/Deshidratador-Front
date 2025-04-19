f930e15e060eb28e9e3a35d4a862b311e0108c9f
DanieZapata <zapataparidaniela@gmail.com>
2025-04-18 23:14:24 -0400

    Add notification view

--- src/pages/NotificationsPage/NotificationsPage.tsx
+++ src/pages/NotificationsPage/NotificationsPage.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+//import React from 'react'
 
 const NotificationsPage = () => {
-  return (
-    <div>NotificationsPage</div>
-  )
-}
+  const data = [
+    { fecha: '17/04/2025 10:00', tipo: 'Alerta', descripcion: 'Temperatura supera 40°C%', estado: 'Pendiente' },
+    { fecha: '17/04/2025 10:10', tipo: 'Informacion', descripcion: 'Datos recibidos correctamente', estado: 'Atendido' },
+  ];
 
-export default NotificationsPage
+  return (
+    <div>
+      <h2 className="text-xl font-bold text-[#173555] pt-8 pb-6">Notificaciones</h2>
+      <div className="bg-white p-6 rounded-xl">
+        <div className="rounded-xl overflow-hidden border border-[#173555]">
+          <table className="min-w-full table-auto border-collapse text-center">
+            <thead className="bg-[#C4D2E7] text-[#173555]">
+              <tr>
+                <th className="py-2 px-4 border-b border-r border-[#173555]">Fecha/Hora</th>
+                <th className="py-2 px-4 border-b border-r border-[#173555]">Tipo</th>
+                <th className="py-2 px-4 border-b border-r border-[#173555]">Descripción</th>
+                <th className="py-2 px-4 border-b border-[#173555]">Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row, index) => {
+                const isLast = index === data.length - 1;
+                const borderBottom = isLast ? '' : 'border-b';
+
+                return (
+                  <tr key={index} className="hover:bg-gray-50">
+                    <td className={`py-2 px-4 border-r border-[#173555] text-center ${borderBottom}`}>{row.fecha}</td>
+                    <td className={`py-2 px-4 border-r border-[#173555] text-center ${borderBottom}`}>{row.tipo}</td>
+                    <td className={`py-2 px-4 border-r border-[#173555] text-left ${borderBottom}`}>{row.descripcion}</td>
+                    <td className={`py-2 px-4 border-[#173555] text-center ${borderBottom}`}>{row.estado}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationsPage;
